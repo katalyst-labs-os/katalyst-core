5acf55a503399e5cf8bc6d7b6d0715df1aa2d40f
Anicet Nougaret <an.nougaret@gmail.com>
2024-09-02 13:52:48 +0200

    setup install requires

--- setup.py
+++ setup.py
@@ -14,6 +14,23 @@ setup(
     url="https://github.com/katalyst-labs-os",
     packages=find_packages(),
     install_requires=[
+        "python-dotenv",
+        "openai",
+        "loguru",
+        "vtk",
+        "numpy==1.26.4",
+        "build123d",
+        "airfoils",
+        "bd-warehouse @ git+https://github.com/gumyr/bd_warehouse",
+        "sentence-transformers==3.0.1",
+        "pymupdf",
+        "tiktoken",
+        "pandas",
+        "lancedb==0.9.0",
+        "opencv-python==4.10.0.84",
+        "open_clip_torch==2.26.1",
+        "langchain-community==0.2.7",
+        "beautifulsoup4"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
