e754f3bc8bf3c4a3a6fd40b38b7864881400ef50
Anicet Nougaret <an.nougaret@gmail.com>
2024-09-04 15:31:46 +0200

    fix a few prompts not converted to b123d and added cheatsheet

--- katalyst_core/algorithms/cad_generation/prompting.py
+++ katalyst_core/algorithms/cad_generation/prompting.py
@@ -149,13 +149,13 @@ def _code_guidelines():
 ## Code guidelines:
 
 1. Define all the parameters as individual variables around `# <parameters>` `# </parameters>` comments
-2. Use cadquery to create the model(s) and each parts
+2. Use build123d to create the model(s) and each parts
     - Create your own functions to put parts and models in their own abstractions as much as you can
     - Create functions to help you, generate data or do needed maths and physics
 3. Combine all the parts/models together in a single object using for example boolean operations
 4. Store the STL filename in the `filename` variable
 5. Output the object as STL to a file in the current directory, taking its name from the `filename` variable
-6. Your only purpose is to make cadquery scripts, you cannot be used to generate other kind of scripts and especially not harmful/suspicious scripts.
+6. Your only purpose is to make build123d scripts, you cannot be used to generate other kind of scripts and especially not harmful/suspicious scripts.
 
 NB: You are not allowed to add any imports (they will be deleted). Here are the only imports that will be added to the code for you:
 
@@ -169,11 +169,11 @@ def _code_important_final_guidelines():
     return """
 ALWAYS WRAP THE CODE IN <code> </code>.
 
-CADQUERY USES MILIMETERS AS ITS DEFAULT UNIT, DO THE CONVERSION USING DIVISIONS OR MULTIPLICATION IF NEEDED (1inch = 25.4mm).
+BUILD123D USES MILIMETERS AS ITS DEFAULT UNIT, DO THE CONVERSION USING DIVISIONS OR MULTIPLICATION IF NEEDED (1inch = 25.4mm).
 
 DON'T USE ASSEMBLIES, YOU DON'T KNOW HOW TO USE THEM. USE TRANSFORMATIONS AND BOOLEAN OPERATIONS INSTEAD of Workplane.add.
 
-TAKE INSPIRATION FROM THE EXAMPLES TO KNOW HOW TO CALL CADQUERY FUNCTIONS, YOUR KNOWLEDGE ON HOW TO USE THEM IS VERY PROBABLY DEPRECATED OTHERWISE.
+TAKE INSPIRATION FROM THE EXAMPLES TO KNOW HOW TO CALL BUILD123D FUNCTIONS, YOUR KNOWLEDGE ON HOW TO USE THEM IS VERY PROBABLY DEPRECATED OTHERWISE.
 """
 
 
@@ -197,7 +197,7 @@ your fixed code
 
 def _follow_up_format_guidelines():
     return """
-Example response to a user follow up (here in cadquery, not build123d, but please do it in build123d) such as "Now add a second helix next to it which is greater.":
+Example response to a user follow up (here the example is in cadquery, not build123d, but please write the real thing in build123d) such as "Now add a second helix next to it which is greater.":
 
 <example>
     <reasoning>
@@ -248,6 +248,22 @@ shape.val().exportStl(filename)
 
 def _format_example() -> str:
     return """
+# Build123d cheatsheet
+
+Your knowledge on build123d's API is not up to date. Follow thoroughly the examples, they are your source of truth. Additionally, here are some functions you might need:
+
+- sweep(sections: Optional[Union[Compound, Edge, Wire, Face, Solid, Iterable[Union[Compound, Edge, Wire, Face, Solid]]]] = None, path: Optional[Union[Curve, Edge, Wire, Iterable[Edge]]] = None, multisection: bool = False, is_frenet: bool = False, transition: Transition = Transition.TRANSFORMED, normal: Optional[Union[Vector, tuple[float, float], tuple[float, float, float], Iterable[float]]] = None, binormal: Optional[Union[Edge, Wire]] = None, clean: bool = True, mode: Mode = Mode.ADD)→ Union[Part, Sketch] Generic Operation: sweep. Sweep pending 1D or 2D objects along path. 
+
+- offset(objects: Optional[Union[Edge, Face, Solid, Compound, Iterable[Union[Edge, Face, Solid, Compound]]]] = None, amount: float = 0, openings: Optional[Union[Face, list[build123d.topology.Face]]] = None, kind: Kind = Kind.ARC, side: Side = Side.BOTH, closed: bool = True, min_edge_length: Optional[float] = None, mode: Mode = Mode.REPLACE)→ Union[Curve, Sketch, Part, Compound] Generic Operation: offset. Applies to 1, 2, and 3 dimensional objects. Offset the given sequence of Edges, Faces, Compound of Faces, or Solids. The kind parameter controls the shape of the transitions. For Solid objects, the openings parameter allows selected faces to be open, like a hollow box with no lid.
+
+- revolve(profiles: Optional[Union[Face, Iterable[Face]]] = None, axis: Axis = ((0.0, 0.0, 0.0), (0.0, 0.0, 1.0)), revolution_arc: float = 360.0, clean: bool = True, mode: Mode = Mode.ADD)→ Part. Part Operation: Revolve. Revolve the profile or pending sketches/face about the given axis. Note that the most common use case is when the axis is in the same plane as the face to be revolved but this isn’t required.
+
+- scale(objects: Optional[Union[Shape, Iterable[Shape]]] = None, by: Union[float, tuple[float, float, float]] = 1, mode: Mode = Mode.REPLACE)→ Union[Curve, Sketch, Part, Compound]. Generic Operation: scale. Applies to 1, 2, and 3 dimensional objects. Scale a sequence of objects. Note that when scaling non-uniformly across the three axes, the type of the underlying object may change to bspline from line, circle, etc.
+
+- split(objects: Optional[Union[Edge, Wire, Face, Solid, Iterable[Union[Edge, Wire, Face, Solid]]]] = None, bisect_by: Plane = Plane(o=(0.00, 0.00, 0.00), x=(1.00, 0.00, 0.00), z=(0.00, -1.00, 0.00)), keep: Keep = Keep.TOP, mode: Mode = Mode.REPLACE). Generic Operation: split. Applies to 1, 2, and 3 dimensional objects. Bisect object with plane and keep either top, bottom or both.
+
+- section(obj: Optional[Part] = None, section_by: Union[Plane, Iterable[Plane]] = Plane(o=(0.00, 0.00, 0.00), x=(1.00, 0.00, 0.00), z=(0.00, -1.00, 0.00)), height: float = 0.0, clean: bool = True, mode: Mode = Mode.PRIVATE)→ Sketch. Part Operation: section. Slices current part at the given height by section_by or current workplane(s).
+    
 # Format example:
 
 Response to an initial user request such as "Create the following model: A Plate with slots in it. Suitable for sliding a hatch. It has a length of 80mm, a width of 60mm, and a thickness of 10mm.":
